6a9858cf56d22a175211a356c812a413fec70d19
mmingoo <aldrn0323@naver.com>
2024-10-11 20:49:21 +0900

    테스트

---
+++ src/main/java/sutdy/dockerhubcicd/DockerHubCiCdApplication.java
@@ -0,0 +1,13 @@
+package sutdy.dockerhubcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerHubCiCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerHubCiCdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sutdy/dockerhubcicd/DockerHubCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package sutdy.dockerhubcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerHubCiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
