227eca5d46f047a1ba4aa49ea6feac65f757e098
mmingoo <aldrn0323@naver.com>
2024-10-11 21:39:10 +0900

    test Controller 생성

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
-FROM ubuntu:latest
-LABEL authors="minyoung"
+# 베이스 이미지로 OpenJDK 17 사용
+FROM openjdk:17-jdk-slim
 
-ENTRYPOINT ["top", "-b"]
+# 작업 디렉토리 설정
+WORKDIR /app
+
+# JAR 파일 복사
+COPY build/libs/*.jar app.jar
+
+# 컨테이너 실행 시 실행할 명령
+ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ src/main/java/sutdy/dockerhubcicd/TestController.java
@@ -0,0 +1,13 @@
+package sutdy.dockerhubcicd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String testController(){
+        return "Hello";
+    }
+}
